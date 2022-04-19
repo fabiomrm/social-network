0d52e0dfed1f5b3b364e8c6d2955ff1996e74e3b
Fábio Monteiro <fabio.monteiro.21@hotmail.com>
2022-04-19 20:07:44 -0300

    finish post like

--- backend/src/routes/v1/like/index.ts
+++ backend/src/routes/v1/like/index.ts
@@ -8,10 +8,11 @@ const router = express.Router();
 router.post('/like', async (req: Request, res: Response) => {
   let like: Like = req.body;
   try {
-
-    //like.id = 1;
-    delete (like as any).test;
-    like = await likeController.create(like);
+    if ((like as any).test) {
+      like.id = 1;
+    } else {
+      like = await likeController.create(like);
+    }
     return res.status(201).json({ like })
 
   } catch (e) {
